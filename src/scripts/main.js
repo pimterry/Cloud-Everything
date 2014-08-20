73e663fff899bfeb8c72d3f9b45dbd5213210304
Tim Perry <tim.perry@softwire.com>
2014-08-20 13:11:11 +0100

    Initial bespoke 1.0 setup

---
+++ src/scripts/main.js
@@ -0,0 +1,26 @@
+// Require Node modules in the browser thanks to Browserify: http://browserify.org
+var bespoke = require('bespoke'),
+  classes = require('bespoke-classes'),
+  keys = require('bespoke-keys'),
+  touch = require('bespoke-touch'),
+  bullets = require('bespoke-bullets'),
+  scale = require('bespoke-scale'),
+  hash = require('bespoke-hash'),
+  state = require('bespoke-state');
+
+// Bespoke.js
+bespoke.from('article', [
+  classes(),
+  keys(),
+  touch(),
+  bullets('li, .bullet'),
+  scale(),
+  hash(),
+  state()
+]);
+
+// Prism syntax highlighting
+// This is actually loaded from "bower_components" thanks to
+// debowerify: https://github.com/eugeneware/debowerify
+require('prism');
+
