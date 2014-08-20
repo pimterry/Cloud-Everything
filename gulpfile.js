3152071094296673bdb6869ef48b4af1bed3b35a
Tim Perry <tim.perry@softwire.com>
2014-08-20 14:15:07 +0100

    Pull in styling from security talk

--- gulpfile.js
+++ gulpfile.js
@@ -57,6 +57,12 @@ gulp.task('images', ['clean:images'], function() {
     .pipe(connect.reload());
 });
 
+gulp.task('fonts', ['clean:fonts'], function() {
+  return gulp.src('src/fonts/**/*')
+    .pipe(gulp.dest('dist/fonts'))
+    .pipe(connect.reload());
+});
+
 gulp.task('clean', function() {
   return gulp.src('dist')
     .pipe(rimraf());
@@ -82,6 +88,11 @@ gulp.task('clean:images', function() {
     .pipe(rimraf());
 });
 
+gulp.task('clean:fonts', function() {
+  return gulp.src('dist/fonts')
+    .pipe(rimraf());
+});
+
 gulp.task('connect', ['build'], function(done) {
   connect.server({
     root: 'dist',
@@ -95,6 +106,7 @@ gulp.task('watch', function() {
   gulp.watch('src/**/*.jade', ['html']);
   gulp.watch('src/styles/**/*.styl', ['css']);
   gulp.watch('src/images/**/*', ['images']);
+  gulp.watch('src/fonts/**/*', ['fonts']);
   gulp.watch([
     'src/scripts/**/*.js',
     'bespoke-theme-*/dist/*.js' // Allow themes to be developed in parallel
@@ -105,6 +117,6 @@ gulp.task('deploy', ['build'], function(done) {
   ghpages.publish(path.join(__dirname, 'dist'), { logger: gutil.log }, done);
 });
 
-gulp.task('build', ['js', 'html', 'css', 'images']);
+gulp.task('build', ['js', 'html', 'css', 'images', 'fonts']);
 gulp.task('serve', ['connect', 'watch']);
 gulp.task('default', ['build']);
